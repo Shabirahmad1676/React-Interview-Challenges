1089ba4d3c60c360ecfffba60680c61bbb2889fd
Shabirahmad1676 <sa3835746@gmail.com>
2025-01-12 17:59:26 +0500

    Completed!

--- FormHandling/src/App.jsx
+++ FormHandling/src/App.jsx
@@ -1,14 +1,54 @@
-// src/App.js
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const [FName, setFName] = useState('')
+  const [email, setemail] = useState('')
+  const [pass, setpass] = useState('')
+  const [conPass, setconPass] = useState('')
+  const [error, seterror] = useState('')
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    if(pass.length<8){
+      seterror('Password must be 8 character!')
+      return;
+    }
+
+    if(pass!=conPass){
+      seterror('Password must match!')
+      return
+    }
+
+    if(!/[!@#$%^&*().,?';":}{]/.test(pass)){
+      seterror('Missing Special Character')
+      return  
+    }
+    
+    if(!/[A_Z]/.test(pass)){
+      seterror("At Least One UpperCase Letter")
+      return
+    }
+
+
+
+    setFName("")
+    setemail("")
+    setpass("")
+    setconPass('')
+    seterror('')
+  }
+
   return (
     <div className="flex items-center justify-center bg-gradient-to-r from-purple-500 to-yellow-300 h-screen">
       
       <div className="flex items-center justify-center w-96 rounded-md p-3 bg-white">
         <div className="w-3/4">
-          <h2 className="text-3xl font-bold mb-6 text-gray-800">Login</h2>
-          <form>
+          <h2 className="text-3xl font-bold mb-6 text-gray-800 text-center bg-gradient-to-r from-yellow-500 to-purple-300 bg-clip-text text-transparent">Create an Account!</h2>
+          <form onSubmit={(e)=>{
+            handleSubmit(e)
+          }}>
+            {/* first name */}
             <div className="mb-6">
               <label
                 htmlFor="username"
@@ -17,13 +57,20 @@ function App() {
                 Full Name
               </label>
               <input
-                type="email"
+              required
+                type="text"
                 id="username"
                 placeholder="Shabir"
+                value={FName}
+                onChange={(e)=>{
+                  setFName(e.target.value)
+                }}
                 className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
               />
             </div>
+            {/* email */}
             <div className="mb-6 flex flex-col">
+
               <label
                 htmlFor="username"
                 className="block text-gray-700 font-medium mb-2"
@@ -31,13 +78,20 @@ function App() {
                 Email
               </label>
               <input
+              required
                 type="email"
                 id="username"
+                value={email}
+                onChange={(e)=>{
+                  setemail(e.target.value)
+                }}
                 placeholder="abc@me.com"
                 className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
               />
             </div>
+            {/* password */}
             <div className="mb-6">
+
               <label
                 htmlFor="password"
                 className="block text-gray-700 font-medium mb-2"
@@ -45,14 +99,42 @@ function App() {
                 Password
               </label>
               <input
+              required
                 type="password"
                 id="password"
+                value={pass}
+                onChange={(e)=>{
+                  setpass(e.target.value)
+                }}
                 placeholder="••••••••"
                 className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
               />
             </div>
            
-          
+            {/* password */}
+            <div className="mb-3">
+
+              <label
+                htmlFor="password"
+                className="block text-gray-700 font-medium mb-2"
+              >
+                Confirm Password
+              </label>
+              <input
+              required
+                type="password"
+                id="password"
+                value={conPass}
+                onChange={(e)=>{
+                  setconPass(e.target.value)
+                }}
+                placeholder="••••••••"
+                className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-500"
+              />
+            </div>
+                {error && (
+                  <p className="text-red-600 font-semibold text-center mb-2">{error}</p>
+                ) }
             <button
               type="submit"
               className="w-full bg-purple-500 text-white py-2 rounded-lg hover:bg-purple-600 transition duration-200"
